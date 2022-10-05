9e25310eeb23731f6d1cdcb9d1bd197627e23797
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2022-10-05 12:54:06 -0500

    Ver3

--- src/main/java/Cifrado_DES/prueba.java
+++ src/main/java/Cifrado_DES/prueba.java
@@ -15,7 +15,8 @@ public static void main (String[] args) throws Exception{
 
     File arc = new File ("C:\\Users\\gussr\\OneDrive\\Documentos\\NetBeansProjects\\DES\\src\\main\\java\\Cifrado_DES\\chi.txt.cifrado");
     Cifrado de = new Cifrado();
-    String chi = de.Descifrar(arc,"penepene");
+String clave = "holahola";
+    String chi = de.Descifrar(arc,clave);
 
     System.out.println("\n"+chi+"\n");
 }
